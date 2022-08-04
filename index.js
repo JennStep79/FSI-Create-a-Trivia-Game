ee4059bf6761aea6f471703bc21a4e2a769297fe
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-08-04 00:24:00 -0700

    added prompts, added user score

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let userName = window.prompt('Please enter your name:')
+let userScore = 0
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
+}
+
+window.alert('Your score is: '+userScore)
+
+
 // Your Code Here
